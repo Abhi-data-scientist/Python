859befd581f2cb9c3fdbb4dd9b86059ae4d9edad
Abhishek <mr.abhi1149@gmail.com>
2025-12-18 13:37:37 +0530

    adsf

--- list/check_sort_or no.py
+++ list/check_sort_or no.py
@@ -4,4 +4,4 @@ for i in range(len(list)-1):
         print("Nhi hai sorted")
         break
 
-    
+    https://github.com/Abhi-data-scientist/Python/tree/6a96579cfc974abc25d290edde38c0a1b008c7f1/Tuple
